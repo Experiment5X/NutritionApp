4c03130df545f1ff90100988c0e775c9264472f7
Adam Spindler <xmeadamx@gmail.com>
2019-03-13 09:42:47 -0400

    Add comments for clarity

--- src/server/app.js
+++ src/server/app.js
@@ -7,10 +7,12 @@ const app = express();
 const NutritionApi = require('./nutrition_api');
 const session = require('express-session');
 
+// determine useful paths in the project directory structure
 const projectRoot = path.join(__dirname, '../..');
 const secretsPath = path.join(projectRoot, 'secrets.json');
 const templatesRoot = path.join(projectRoot, 'src/templates');
 
+// responds to a web request with a rendered mustache template (HTML content)
 function respondWithMustache(res, templateName, context) {
     let searchTemplatePath = path.join(templatesRoot, templateName);
     let searchTemplate = fs.readFileSync(searchTemplatePath, 'utf8');
@@ -19,16 +21,21 @@ function respondWithMustache(res, templateName, context) {
     res.send(html);
 }
 
+// allows us to get the content from a form submission
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// set up the session
 app.use(session({
     secret: 'nutrition-secret'
 }));
 
+// called when user navigates to search page
 app.get('/search', (req, res) => {
     let previousSearch = null;
+
+    // check if the search history has been stored in the session yet
     if (req.session.searchHistory) {
         previousSearch = req.session.searchHistory[req.session.searchHistory.length - 1];
     }
@@ -36,7 +43,9 @@ app.get('/search', (req, res) => {
     respondWithMustache(res, 'search.mustache', {'previousSearch': previousSearch});
 });
 
+// called when user searches for a food (they submit the form)
 app.post('/search', (req, res) => {
+    // handle maintaining the search history
     let searchQuery = req.body.query;
     if (req.session.searchHistory) {
         req.session.searchHistory.push(searchQuery)
@@ -45,6 +54,9 @@ app.post('/search', (req, res) => {
     }
 
     let nutrition = new NutritionApi(secretsPath);
+
+    // ask the nutrition object to hit the government API
+    // and give it a callback to be called when the API has responded
     nutrition.search(searchQuery, (error, results) => {
         if (error) {
             res.send('Error searching: ' + error.toString());
@@ -59,6 +71,7 @@ app.post('/search', (req, res) => {
     });
 });
 
+// start the webserver
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
--- src/server/nutrition_api.js
+++ src/server/nutrition_api.js
@@ -2,22 +2,28 @@ const fs = require('fs');
 const request = require('request');
 
 class NutritionApi {
+    // create a new instance of the NutritionApi object with the path
+    // to the JSON file containing the API key for the government API
     constructor(secretsFile) {
         let fileContents = fs.readFileSync(secretsFile);
         let secrets = JSON.parse(fileContents);
 
-        let apiKey = secrets['nutrition_api_key'];
+        // set up the URL for making requests
+        let apiKey = secrets.nutrition_api_key;
         this.urlRoot = 'https://api.nal.usda.gov/ndb/search/?format=json&api_key=' + apiKey;
     }
 
+    // search for a food and use a callback to get the result
     search(foodSearchString, callback) {
         let searchUrl = this.urlRoot + '&sort=n&max=25&offset=0&q=' + foodSearchString;
+        
+        // hit the API and give it a callback to get the result
         request(searchUrl, (error, response, body) => {
             if (error) {
                 callback(error);
             } else {
                 let parsedResponse = JSON.parse(body);
-                let searchResults = parsedResponse['list']['item'];
+                let searchResults = parsedResponse.list.item;
 
                 let cleanedResults = this.parseSearchResults(searchResults);
                 callback(false, cleanedResults);
@@ -25,6 +31,7 @@ class NutritionApi {
         });
     }
 
+    // cleanup each search result
     parseSearchResults(searchResults) {
         var cleaned = [];
         for (var result of searchResults) {
